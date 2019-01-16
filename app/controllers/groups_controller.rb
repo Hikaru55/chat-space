169c5f103adccae035215b6c04d71f83738f8061
[hikaru55] <[hikarukondo55@icloud.com]>
2019-01-16 15:01:23 +0900

    エラーの修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,6 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
 
-  def index
-  end
-
   def new
     @group = Group.new
     @group.users << current_user
@@ -11,6 +8,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    @group.users << current_user
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
@@ -28,7 +26,7 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, { :user_ids => [] })
+    params.require(:group).permit(:name, { user_ids: [] })
   end
 
   def set_group
